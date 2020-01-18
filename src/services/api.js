80da62ffc90ea538b2603019eb07020d07c7ad60
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-01-18 12:10:45 -0300

    Simplifies api url reference

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,12 @@
 import axios from 'axios';
 
-const generateBaseUrl = () => {
-  const {
-    REACT_APP_SERVER_PROTOCOL: protocol,
-    REACT_APP_SERVER_HOST: host,
-    REACT_APP_SERVER_PORT: port,
-  } = process.env;
-
-  const url = `${protocol}://${host}:${port}`;
+const getServerUrl = () => {
+  const { REACT_APP_SERVER_URL: url } = process.env;
   return url;
 };
 
 const api = axios.create({
-  baseURL: generateBaseUrl(),
+  baseURL: getServerUrl(),
 });
 
 export default api;
