25a95d95786306d10d4c3cdaf821a9613fc664b2
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-01-20 21:17:10 -0300

    Adds internationalization to form

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import DevForm from '../DevForm';
 import { withTranslation } from 'react-i18next';
+import i18n from '../../i18n';
 
-const Sidebar = ({ handleAddDev, formError, t }) => (
-  <aside>
-    <strong>{t('form.title')}</strong>
-    <DevForm onSubmit={handleAddDev} />
-  </aside>
-);
+const Sidebar = ({ handleAddDev, formError, t }) => {
+  const { language } = i18n;
+
+  const setLanguage = (lang) => () => i18n.changeLanguage(lang);
+
+  const setEnLanguage = setLanguage('en');
+  const setPtLanguage = setLanguage('pt');
+
+  const languageSelected = language === 'en' ? 'en-selected' : 'pt-selected';
+
+  return (
+    <aside>
+      <div className="sidebar-header">
+        <strong className="sidebar-title">
+          {t('form.title')}
+        </strong>
+        <div className={`sidebar-language ${languageSelected}`}>
+          <button onClick={setEnLanguage} >EN</button>
+          <span> / </span>
+          <button onClick={setPtLanguage}>PT</button>
+        </div>
+      </div>
+      <DevForm onSubmit={handleAddDev} />
+    </aside>
+  );
+};
 
 export default withTranslation()(Sidebar);
